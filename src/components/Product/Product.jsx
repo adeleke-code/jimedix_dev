cd0a0229325e30e6cb51da2432f9762f465225f2
misspelz <pelumiiadetoye@gmail.com>
2024-05-22 08:14:18 +0100

    added products and fixed page not found

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,43 @@
-
 import { BsCart2 } from "react-icons/bs";
+import Rexitin from "../../assets/jimedix_image5.jpeg";
+import Indications from "../../assets/jimedix_image4.jpeg";
+import Dizolve from "../../assets/jimedix_image3.jpeg";
+import Rextore from "../../assets/jimedix_image2.jpeg";
+
+const products = [
+  {
+    id: 1,
+    image: Rexitin,
+    title: "Rexitin Herbal Product",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt aut quisquam consequatur laborum cupiditate delectus.",
+    price: "#25,000",
+  },
+  {
+    id: 2,
+    image: Indications,
+    title: "Indications for Men",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.",
+    price: "#25,000",
+  },
+  {
+    id: 3,
+    image: Dizolve,
+    title: "Dizolve Herbal Product",
+    description:
+      "Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+    price: "#25,000",
+  },
+  {
+    id: 4,
+    image: Rextore,
+    title: "Rextore Herbal Capsules",
+    description:
+      "Cras ultricies ligula sed magna dictum porta. Proin eget tortor risus. Donec sollicitudin molestie malesuada.",
+    price: "#25,000",
+  },
+];
 
 export const Product = () => {
   return (
@@ -16,33 +54,31 @@ export const Product = () => {
         <b>Quality Guaranteed, Satisfaction Assured!</b>
       </p>
 
-      <div className="mt-10 lg:mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-6 lg:gap-4">
-        {[1, 2, 3, 4].map((index) => (
+      <div className="mt-10 lg:mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-4 shadow-sm">
+        {products.map((product) => (
           <div
-            key={index}
-            className="relative flex flex-col items-center gap-4 custom-hover hover:border-y-2 custom-hover hover:shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] duration-500 cursor-pointer rounded-[20px] group w-[80%] mx-auto lg:w-full"
+            key={product.id}
+            className="relative flex flex-col items-center gap-4 custom-hover hover:border-y-2 shadow-sm border custom-hover hover:shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] duration-500 cursor-pointer rounded-[20px] group w-[80%] mx-auto lg:w-full"
           >
             <img
-              src="https://images.pexels.com/photos/3873209/pexels-photo-3873209.jpeg?auto=compress&cs=tinysrgb&w=600"
-              alt=""
-              className="rounded-t-[20px]"
+              src={product.image}
+              alt={product.title}
+              className="rounded-t-[20px] h-[200px] w-full"
             />
-            <div className=" pb-10">
+            <div className="pb-10">
               <h5 className="text-[14px] lg:text-lg font-bold text-center">
-                Spray Bottles
+                {product.title}
               </h5>
               <p className="text-center text-[14px] lg:text-[16px] text-gray-500 px-8 pb-4 mt-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                aut quisquam consequatur laborum cupiditate delectus.
+                {product.description}
               </p>
-              {/* Cart icon and price (visible only on hover) */}
-              <div className="flex items-center justify-center -space-x-4 mt-4 opacity-0 absolute bottom-0  p-4 w-full invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-300 rounded-b-[20px] -mb-9">
+
+              <div className="flex items-center justify-center -space-x-4 mt-4 opacity-0 absolute bottom-0 p-4 w-full invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-300 rounded-b-[20px] -mb-9">
                 <div className="w-10 h-10 rounded-full bg-primary flex items-center justify-center z-20">
                   <BsCart2 size={20} color="#fff" />
                 </div>
-                <div className="text-primary h-8 w-20 rounded-[30px] bg-[#fff] flex items-center justify-center shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] pl-3 font-bold">
-                  {" "}
-                  $10.99
+                <div className="text-primary h-8 w-24 rounded-[30px] bg-[#fff] flex items-center justify-center shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] pl-3 font-bold">
+                  {product.price}
                 </div>
               </div>
             </div>
